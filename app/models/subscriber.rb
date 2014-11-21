8e58dd2452a9b2e414cc2d8b59107d5ecea7fb80
Nick Bernard <nick@nrbernard.com>
2014-11-21 03:27:59 -0800

    Use Mailgun API to send email

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -9,6 +9,12 @@ class Subscriber < ActiveRecord::Base
 private
 
   def send_subscriber_email
-    SubscriberMailer.new_subscriber(self).deliver
+    # SubscriberMailer.new_subscriber(self).deliver
+
+    RestClient.post "https://api:#{ENV['MAILGUN_API_KEY']}@api.mailgun.net/v2/sandbox8996eb7a622440389f7706e641323fe9.mailgun.org/messages",
+    :from => "#{email}",
+    :to => "pushboardportland@gmail.com",
+    :subject => "New Subscriber",
+    :text => "Email: #{email}\nName: #{name}"
   end
 end
