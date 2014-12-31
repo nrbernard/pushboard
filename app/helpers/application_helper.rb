bd9f04163e4c23d6dacab05a1696ee321dcb1c5a
Nick Bernard <nick@nrbernard.com>
2014-12-31 03:24:39 -0800

    Add page titles and descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title(page_title)
+    content_for :title, page_title.to_s
+  end
+
+  def description(page_description)
+    content_for :description, page_description.to_s
+  end
+
 end
