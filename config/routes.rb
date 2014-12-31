bd1f13b97325bcd858be5f2598ebeb2f20486459
Nick Bernard <nick@nrbernard.com>
2014-12-30 19:20:32 -0800

    Display submitted events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "static_pages#index"
 
-  resources :subscribers, only: [:index, :new]
-  resources :events, only: [:index, :new]
+  resources :subscribers, only: [:index, :new, :create]
+  resources :events, only: [:index, :new, :create]
 end
